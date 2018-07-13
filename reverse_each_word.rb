d181fe4bda8f70e0d1a651a14701177ff930ec76
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:11:43 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(string)
-  puts split_arr = string.split(" ").collect {|word| word.reverse}.join(" ")
+  return split_arr = string.split(" ").collect {|word| word.reverse}.join(" ")
 end
 
 
